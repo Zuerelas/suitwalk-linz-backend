bdb8270aae370b9a32efc420d3ceb2fcf547df04
Elias Pöschl <eliaspoe194@gmail.com>
2025-05-18 20:45:00 +0200

    Refactor CORS middleware to allow specific origins without defaulting to '*'

--- server.js
+++ server.js
@@ -77,18 +77,16 @@ app.use((req, res, next) => {
 
 // CORS Configuration - Allow specific domains
 app.use((req, res, next) => {
-    const allowedOrigins = ['https://test.suitwalk-linz.at', 'https://suitwalk-linz.at'];
-    const origin = req.headers.origin;
-    
-    if (allowedOrigins.includes(origin)) {
-        res.header("Access-Control-Allow-Origin", origin);
-    } else {
-        res.header("Access-Control-Allow-Origin", "*");
-    }
-    
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
-    next();
+  const allowedOrigins = ['https://test.suitwalk-linz.at', 'https://suitwalk-linz.at'];
+  const origin = req.headers.origin;
+
+  if (allowedOrigins.includes(origin)) {
+    res.header("Access-Control-Allow-Origin", origin);
+  }
+
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  next();
 });
 
 // Validate required environment variables - more lenient for Vercel
